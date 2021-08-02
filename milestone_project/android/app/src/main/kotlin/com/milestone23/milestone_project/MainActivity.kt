a626a8398219e3ce20ddff1fa98f8f7b537b5b90
oohdimas <dimasfaid@gmail.com>
2021-08-02 22:54:51 +0700

    First Commit, project kosong

---
+++ milestone_project/android/app/src/main/kotlin/com/milestone23/milestone_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.milestone23.milestone_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ milestone_project/lib/Screen_1.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class Screen_1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
