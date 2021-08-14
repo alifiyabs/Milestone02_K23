39b1eead521ac08701342b35364a23bfd9a33660
Dimas Faidh Muzaki <dimasfaid@gmail.com>
2021-08-14 19:19:15 +0700

    rapihin screen rekomendasi

--- milestone_lokalin/lib/ScreenRekomendasi.dart
+++ milestone_lokalin/lib/ScreenRekomendasi.dart
@@ -9,80 +9,11 @@ class ScreenRekomendasi extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(165),
-        child: AppBar(
-          iconTheme: IconThemeData(color: Colors.black),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(30),
-            ),
-          ),
-          backgroundColor: Color(0xffECCD4F),
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Daerahmu",
-                style: TextStyle(
-                    fontFamily: "InterBlack",
-                    fontSize: 18,
-                    color: Colors.black),
-              ),
-              Text("Jl. Makmur Jaya 1 No. 10",
-                  style: TextStyle(
-                      fontFamily: "InterBlack",
-                      fontSize: 14,
-                      color: Colors.black))
-            ],
-          ),
-          flexibleSpace: Container(
-            margin: EdgeInsets.fromLTRB(10, 80, 10, 0),
-            child: Column(children: [
-              TextField(
-                textAlign: TextAlign.start,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.black,
-                  ),
-                  hintText: '',
-                  hintStyle: TextStyle(
-                    fontSize: 16,
-                    fontFamily: "InterMedium",
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(40),
-                    borderSide: BorderSide(
-                      width: 0,
-                      style: BorderStyle.none,
-                    ),
-                  ),
-                  filled: true,
-                  contentPadding: EdgeInsets.all(4),
-                  fillColor: Colors.white,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 15),
-                child: Text(
-                  "Rekomendasi UMKM untukmu",
-                  style: TextStyle(fontFamily: "InterBlack", fontSize: 24),
-                ),
-              ),
-            ]),
-          ),
-        ),
-      ),
+          preferredSize: Size.fromHeight(165), child: AppBarRekomendasi()),
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the Drawer if there isn't enough vertical
-        // space to fit everything.
         child: Container(
           decoration: BoxDecoration(color: Colors.white),
           child: Column(
-            // Important: Remove any padding from the ListView.
-
             children: <Widget>[
               Expanded(
                 child: Column(
@@ -199,41 +130,113 @@ class ScreenRekomendasi extends StatelessWidget {
                     mainAxisSpacing: 8,
                     children: tourismPlaceList.map((place) {
                       return InkWell(
-                        onTap: () {},
-                        child: Card(
-                          child: Container(
-                            height: 300,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15.0),
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: AssetImage(place.imageAsset))),
-                            child: Align(
-                              alignment: Alignment.bottomLeft,
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 10, bottom: 10),
-                                child: Text(
-                                  place.name,
-                                  style: TextStyle(
-                                      fontFamily: "InterMedium",
-                                      fontWeight: FontWeight.w200,
-                                      fontSize: 14,
-                                      color: Color(0xffffffff)),
-                                ),
-                              ),
-                            ),
-                          ),
-                          margin:
-                              EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0),
-                        ),
-                      );
+                          onTap: () {}, child: CardRekomendasi(place: place));
                     }).toList(),
                   ),
                 ),
               ))
         ],
+      ),
+    );
+  }
+}
+
+class CardRekomendasi extends StatelessWidget {
+  const CardRekomendasi({Key? key, required this.place}) : super(key: key);
+  final place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        height: 300,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+            image: DecorationImage(
+                fit: BoxFit.cover, image: AssetImage(this.place.imageAsset))),
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10, bottom: 10),
+            child: Text(
+              place.name,
+              style: TextStyle(
+                  fontFamily: "InterMedium",
+                  fontWeight: FontWeight.w200,
+                  fontSize: 14,
+                  color: Color(0xffffffff)),
+            ),
+          ),
+        ),
+      ),
+      margin: EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0),
+    );
+  }
+}
+
+class AppBarRekomendasi extends StatelessWidget {
+  const AppBarRekomendasi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      iconTheme: IconThemeData(color: Colors.black),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(30),
+        ),
+      ),
+      backgroundColor: Color(0xffECCD4F),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Daerahmu",
+            style: TextStyle(
+                fontFamily: "InterBlack", fontSize: 18, color: Colors.black),
+          ),
+          Text("Jl. Makmur Jaya 1 No. 10",
+              style: TextStyle(
+                  fontFamily: "InterBlack", fontSize: 14, color: Colors.black))
+        ],
+      ),
+      flexibleSpace: Container(
+        margin: EdgeInsets.fromLTRB(10, 80, 10, 0),
+        child: Column(children: [
+          TextField(
+            textAlign: TextAlign.start,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              hintText: '',
+              hintStyle: TextStyle(
+                fontSize: 16,
+                fontFamily: "InterMedium",
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(40),
+                borderSide: BorderSide(
+                  width: 0,
+                  style: BorderStyle.none,
+                ),
+              ),
+              filled: true,
+              contentPadding: EdgeInsets.all(4),
+              fillColor: Colors.white,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15),
+            child: Text(
+              "Rekomendasi UMKM untukmu",
+              style: TextStyle(fontFamily: "InterBlack", fontSize: 24),
+            ),
+          ),
+        ]),
       ),
     );
   }
