3237a2c9d370fce7daf825c7a5f4d98d6c709014
alifiyabs <81351902+alifiyabs@users.noreply.github.com>
2021-08-15 21:28:17 +0700

    data umkm (REVISI)

--- milestone_lokalin/lib/umkm.dart
+++ milestone_lokalin/lib/umkm.dart
@@ -22,161 +22,192 @@ var umkm_list = [
   Umkm(
       nama_umkm: "Dash Woman's Outfit",
       alamat: "Jl.P & K Gg. Pisangan",
-      image_url: "https://sc01.alicdn.com/kf/HTB1uGjyeljTBKNjSZFuq6z0HFXaG/202439437/HTB1uGjyeljTBKNjSZFuq6z0HFXaG.jpg",
+      image_url:
+          "https://sc01.alicdn.com/kf/HTB1uGjyeljTBKNjSZFuq6z0HFXaG/202439437/HTB1uGjyeljTBKNjSZFuq6z0HFXaG.jpg",
       no_telp: "081977453250",
       daerah: "Jakarta Selatan",
-      kategori: "Barang"
-      url_lokasi: "https://www.tokopedia.com/4tuna")
+      kategori: "Barang",
+      url_lokasi: "https://www.tokopedia.com/4tuna"),
   Umkm(
-      nama_umkm: "Toko Batu Alam Raden", 
-      alamat: "Jl.Kemang Pratama Raya", 
-      image_url: "https://lh3.googleusercontent.com/p/AF1QipPYmvXqVgskSYIBtxmkdH99GB1nPF8oCzsztkBf=s1280-p-no-v1", 
-      no_telp: "081397452633", 
-      daerah: "Kota Bekasi", 
-      kategori: "Barang"
-      url_lokasi: "https://www.tokopedia.com/grosirbatualam")
+      nama_umkm: "Toko Batu Alam Raden",
+      alamat: "Jl.Kemang Pratama Raya",
+      image_url:
+          "https://lh3.googleusercontent.com/p/AF1QipPYmvXqVgskSYIBtxmkdH99GB1nPF8oCzsztkBf=s1280-p-no-v1",
+      no_telp: "081397452633",
+      daerah: "Kota Bekasi",
+      kategori: "Barang",
+      url_lokasi: "https://www.tokopedia.com/grosirbatualam"),
   Umkm(
-      nama_umkm: "Toko Krey Ling", 
-      alamat: "Jl.Hasyim Ashari", 
-      image_url: "https://awningjayalana.com/wp-content/uploads/2018/11/canopy-kain-1-394x258.png", 
-      no_telp: "081634562480", 
-      daerah: "Jakarta Pusat", 
-      kategori: "Barang"
-      url_lokasi: "https://shopee.co.id/yuliyah_09?categoryId=100636&itemId=8308897127")
+      nama_umkm: "Toko Krey Ling",
+      alamat: "Jl.Hasyim Ashari",
+      image_url:
+          "https://awningjayalana.com/wp-content/uploads/2018/11/canopy-kain-1-394x258.png",
+      no_telp: "081634562480",
+      daerah: "Jakarta Pusat",
+      kategori: "Barang",
+      url_lokasi:
+          "https://shopee.co.id/yuliyah_09?categoryId=100636&itemId=8308897127"),
   Umkm(
-      nama_umkm: "Ayam Geprek Pedas", 
-      alamat: "Jl.Sonton 100", 
-      image_url: "http://kbu-cdn.com/dk/wp-content/uploads/tahu-crispy-geprek.jpg", 
-      no_telp: "081426557899", 
-      daerah: "Jakarta Selatan", 
-      kategori: "Makanan"
-      url_lokasi: "https://food.grab.com/id/en/restaurant/ayam-geprek-omema-tebet-delivery/6-CZLENBBFVECKGA")
+      nama_umkm: "Ayam Geprek Pedas",
+      alamat: "Jl.Sonton 100",
+      image_url:
+          "http://kbu-cdn.com/dk/wp-content/uploads/tahu-crispy-geprek.jpg",
+      no_telp: "081426557899",
+      daerah: "Jakarta Selatan",
+      kategori: "Makanan",
+      url_lokasi:
+          "https://food.grab.com/id/en/restaurant/ayam-geprek-omema-tebet-delivery/6-CZLENBBFVECKGA"),
   Umkm(
-      nama_umkm: "Bakti Raya Cetak", 
-      alamat: "Jl.Ciputat Raya (Depan Stasiun Kebayoran Lama)", 
-      image_url: "https://iqbalazhari.com/wp-content/uploads/2016/10/peluang-usaha-toko-percetakan.jpg", 
-      no_telp: "081645224788", 
-      daerah: "Jakarta Selatan", 
-      kategori: "Barang"
-      url_lokasi: "https://www.tokopedia.com/imindopedia")
+      nama_umkm: "Bakti Raya Cetak",
+      alamat: "Jl.Ciputat Raya (Depan Stasiun Kebayoran Lama)",
+      image_url:
+          "https://iqbalazhari.com/wp-content/uploads/2016/10/peluang-usaha-toko-percetakan.jpg",
+      no_telp: "081645224788",
+      daerah: "Jakarta Selatan",
+      kategori: "Barang",
+      url_lokasi: "https://www.tokopedia.com/imindopedia"),
   Umkm(
-      nama_umkm: "Romy Mebel", 
-      alamat: "Jl.Pondok Pinang Raya", 
-      image_url: "https://www.serbabandung.com/wp-content/uploads/2016/02/Toko-Mebel-Jati-Mulya.jpg", 
-      no_telp: "081654228799", 
-      daerah: "Jakarta Selatan", 
-      kategori: "Barang"
-      url_lokasi: "https://www.tokopedia.com/bezatishfurniture")
+      nama_umkm: "Romy Mebel",
+      alamat: "Jl.Pondok Pinang Raya",
+      image_url:
+          "https://www.serbabandung.com/wp-content/uploads/2016/02/Toko-Mebel-Jati-Mulya.jpg",
+      no_telp: "081654228799",
+      daerah: "Jakarta Selatan",
+      kategori: "Barang",
+      url_lokasi: "https://www.tokopedia.com/bezatishfurniture"),
   Umkm(
-      nama_umkm: "Aneka Masakan Tempe Bu Ani", 
-      alamat: "Jl.Utan Panjang", 
-      image_url: "https://cdn.idntimes.com/content-images/community/2021/01/fromandroid-e578c77e38210897194db9cd8510663b.jpg", 
-      no_telp: "081276945730", 
-      daerah: "Jakarta Pusat", 
-      kategori: "Makanan"
-      url_lokasi: "https://food.grab.com/id/en/restaurant/mendoan-yushi-harapan-mulya-delivery/6-C2ACNBW1EU2YJX")
+      nama_umkm: "Aneka Masakan Tempe Bu Ani",
+      alamat: "Jl.Utan Panjang",
+      image_url:
+          "https://cdn.idntimes.com/content-images/community/2021/01/fromandroid-e578c77e38210897194db9cd8510663b.jpg",
+      no_telp: "081276945730",
+      daerah: "Jakarta Pusat",
+      kategori: "Makanan",
+      url_lokasi:
+          "https://food.grab.com/id/en/restaurant/mendoan-yushi-harapan-mulya-delivery/6-C2ACNBW1EU2YJX"),
   Umkm(
-      nama_umkm: "Tempe Bacem Jakarta", 
-      alamat: "Jl.Irian", 
-      image_url: "https://doyanresep.com/wp-content/uploads/2020/06/resep-tempe-bacem.jpg", 
-      no_telp: "081765248766", 
-      daerah: "Jakarta Pusat", 
-      kategori: "Makanan"
-      url_lokasi: "https://food.grab.com/id/en/restaurant/gudeg-jogja-mbak-anung-kemayoran-delivery/6-CZDZJJXFJ6UZA6")
+      nama_umkm: "Tempe Bacem Jakarta",
+      alamat: "Jl.Irian",
+      image_url:
+          "https://doyanresep.com/wp-content/uploads/2020/06/resep-tempe-bacem.jpg",
+      no_telp: "081765248766",
+      daerah: "Jakarta Pusat",
+      kategori: "Makanan",
+      url_lokasi:
+          "https://food.grab.com/id/en/restaurant/gudeg-jogja-mbak-anung-kemayoran-delivery/6-CZDZJJXFJ6UZA6"),
   Umkm(
-     nama_umkm: "Pusat Celana Ammi", 
-     alamat: "Jl.Letjen Suprapto", 
-     image_url: "http://asset-a.grid.id/crop/0x0:0x0/x/photo/2019/01/23/1368966398.jpg", 
-     no_telp: "081423668799", 
-     daerah: "Jakarta Pusat", 
-     kategori: "Barang"
-     url_lokasi: "https://www.tokopedia.com/pakaianmurah92")
+      nama_umkm: "Pusat Celana Ammi",
+      alamat: "Jl.Letjen Suprapto",
+      image_url:
+          "http://asset-a.grid.id/crop/0x0:0x0/x/photo/2019/01/23/1368966398.jpg",
+      no_telp: "081423668799",
+      daerah: "Jakarta Pusat",
+      kategori: "Barang",
+      url_lokasi: "https://www.tokopedia.com/pakaianmurah92"),
   Umkm(
-     nama_umkm: "Konveksi Bu Adi", 
-     alamat: "Jl.Serdang Kemayoran", 
-     image_url: "https://alamatjalan.com/wp-content/uploads/2018/12/Toko-Harapan-Makassar-Toko-Kain-dan-Perlengpan-Menjahit-min.jpg", 
-     no_telp: "081945772877", 
-     daerah: "Jakarta Pusat", 
-     kategori: "Barang"
-     url_lokasi: "https://shopee.co.id/roditex?categoryId=100017&itemId=6959923838")
+      nama_umkm: "Konveksi Bu Adi",
+      alamat: "Jl.Serdang Kemayoran",
+      image_url:
+          "https://alamatjalan.com/wp-content/uploads/2018/12/Toko-Harapan-Makassar-Toko-Kain-dan-Perlengpan-Menjahit-min.jpg",
+      no_telp: "081945772877",
+      daerah: "Jakarta Pusat",
+      kategori: "Barang",
+      url_lokasi:
+          "https://shopee.co.id/roditex?categoryId=100017&itemId=6959923838"),
   Umkm(
-     nama_umkm: "YourShoes", 
-     alamat: "Jl.Kemayoran", 
-     image_url: "https://i2.wp.com/www.artforia.com/wp-content/uploads/2019/08/worm-tokyo-in-store.jpg?ssl=1", 
-     no_telp: "081745367855", 
-     daerah: "Jakarta Pusat", 
-     kategori: "Barang"
-     url_lokasi: "https://shopee.co.id/wcf6xbamlq?categoryId=100532&itemId=9023474264")
+      nama_umkm: "YourShoes",
+      alamat: "Jl.Kemayoran",
+      image_url:
+          "https://i2.wp.com/www.artforia.com/wp-content/uploads/2019/08/worm-tokyo-in-store.jpg?ssl=1",
+      no_telp: "081745367855",
+      daerah: "Jakarta Pusat",
+      kategori: "Barang",
+      url_lokasi:
+          "https://shopee.co.id/wcf6xbamlq?categoryId=100532&itemId=9023474264"),
   Umkm(
-     nama_umkm: "Warung Ayam Bakar Adi", 
-     alamat: "Jl.Mampang Prapatan 7", 
-     image_url: "https://selerasa.com/wp-content/uploads/2015/12/images_daging_ayam-panggang-bumbu-spesial.jpg", 
-     no_telp: "081465884622", 
-     daerah: "Jakarta Selatan", 
-     kategori: "Makanan"
-     url_lokasi: "https://food.grab.com/id/en/restaurant/ayam-bakar-rizki-abr-mampang-mampang-prapatan-delivery/6-CZBVGCC2GB4HJE")
+      nama_umkm: "Warung Ayam Bakar Adi",
+      alamat: "Jl.Mampang Prapatan 7",
+      image_url:
+          "https://selerasa.com/wp-content/uploads/2015/12/images_daging_ayam-panggang-bumbu-spesial.jpg",
+      no_telp: "081465884622",
+      daerah: "Jakarta Selatan",
+      kategori: "Makanan",
+      url_lokasi:
+          "https://food.grab.com/id/en/restaurant/ayam-bakar-rizki-abr-mampang-mampang-prapatan-delivery/6-CZBVGCC2GB4HJE"),
   Umkm(
-     nama_umkm: "Kopi Seberang Kota", 
-     alamat: "Jl.Warung Buncit III", 
-     image_url: "https://majalah.ottencoffee.co.id/wp-content/uploads/2016/11/kedai-kopi.jpg", 
-     no_telp: "081478994500", 
-     daerah: "Jakarta Selatan", 
-     kategori: "Minuman"
-     url_lokasi: "https://food.grab.com/id/en/restaurant/kopi-koup-tebet-timur-delivery/6-CZLJPEJUEA3ENE")
+      nama_umkm: "Kopi Seberang Kota",
+      alamat: "Jl.Warung Buncit III",
+      image_url:
+          "https://majalah.ottencoffee.co.id/wp-content/uploads/2016/11/kedai-kopi.jpg",
+      no_telp: "081478994500",
+      daerah: "Jakarta Selatan",
+      kategori: "Minuman",
+      url_lokasi:
+          "https://food.grab.com/id/en/restaurant/kopi-koup-tebet-timur-delivery/6-CZLJPEJUEA3ENE"),
   Umkm(
-     nama_umkm: "Kids Friendly", 
-     alamat: "Jl.UPT Kalibata Pulo", 
-     image_url: "https://www.birdsnbees.co.id/blog/wp-content/uploads/2020/07/Toko-Jual-Baju-Anak-dan-Bayi-011.jpg", 
-     no_telp: "081645448755", 
-     daerah: "Jakarta Selatan", 
-     kategori: "Barang"
-     url_lokasi: "https://www.tokopedia.com/mon-cheri-id")
+      nama_umkm: "Kids Friendly",
+      alamat: "Jl.UPT Kalibata Pulo",
+      image_url:
+          "https://www.birdsnbees.co.id/blog/wp-content/uploads/2020/07/Toko-Jual-Baju-Anak-dan-Bayi-011.jpg",
+      no_telp: "081645448755",
+      daerah: "Jakarta Selatan",
+      kategori: "Barang",
+      url_lokasi: "https://www.tokopedia.com/mon-cheri-id"),
   Umkm(
-     nama_umkm: "Umar Peci Jakarta", 
-     alamat: "Jl.H. Buang No.25", 
-     image_url: "https://cdn-2.tstatic.net/wartakota/foto/bank/images/20180528berita-foto-pedagang-kopiah-tanah-abang-mendulang-untung1_20180528_170408.jpg", 
-     no_telp: "083547229688", 
-     daerah: "Jakarta Selatan", 
-     kategori: "Barang"
-     url_lokasi: "https://www.tokopedia.com/anekakoko") 
+      nama_umkm: "Umar Peci Jakarta",
+      alamat: "Jl.H. Buang No.25",
+      image_url:
+          "https://cdn-2.tstatic.net/wartakota/foto/bank/images/20180528berita-foto-pedagang-kopiah-tanah-abang-mendulang-untung1_20180528_170408.jpg",
+      no_telp: "083547229688",
+      daerah: "Jakarta Selatan",
+      kategori: "Barang",
+      url_lokasi: "https://www.tokopedia.com/anekakoko"),
   Umkm(
-     nama_umkm: "TasMu", 
-     alamat: "Jl.Bumi Raya", 
-     image_url: "https://cf.shopee.co.id/file/2b28b278a0a4e34348f4323f8faf848f", 
-     no_telp: "081176220866", 
-     daerah: "Jakarta Timur", 
-     kategori: "Barang"
-     url_lokasi: "https://shopee.co.id/dnv.store?categoryId=100016&itemId=7650213155") 
+      nama_umkm: "TasMu",
+      alamat: "Jl.Bumi Raya",
+      image_url:
+          "https://cf.shopee.co.id/file/2b28b278a0a4e34348f4323f8faf848f",
+      no_telp: "081176220866",
+      daerah: "Jakarta Timur",
+      kategori: "Barang",
+      url_lokasi:
+          "https://shopee.co.id/dnv.store?categoryId=100016&itemId=7650213155"),
   Umkm(
-     nama_umkm: "Art of Binder", 
-     alamat: "Jl.Tanah Rendah Kebon Pala 1", 
-     image_url: "https://cf.shopee.co.id/file/0500e8d46c0b9a4d0cb2f166db00b8cb_tn", 
-     no_telp: "081723997644", 
-     daerah: "Jakarta Timur", 
-     kategori: "Barang"
-     url_lokasi: "https://shopee.co.id/artistik_pro?categoryId=100638&itemId=3519469567") 
+      nama_umkm: "Art of Binder",
+      alamat: "Jl.Tanah Rendah Kebon Pala 1",
+      image_url:
+          "https://cf.shopee.co.id/file/0500e8d46c0b9a4d0cb2f166db00b8cb_tn",
+      no_telp: "081723997644",
+      daerah: "Jakarta Timur",
+      kategori: "Barang",
+      url_lokasi:
+          "https://shopee.co.id/artistik_pro?categoryId=100638&itemId=3519469567"),
   Umkm(
-     nama_umkm: "FunPlay Store", 
-     alamat: "Jl.Sunter Jaya", 
-     image_url: "https://cf.shopee.co.id/file/4150b8ecc8b19ea3e3ac2c47e3845ef9", 
-     no_telp: "081722445674", 
-     daerah: "Jakarta Utara", 
-     kategori: "Barang"
-     url_lokasi: "https://shopee.co.id/animeyu.case?categoryId=100639&itemId=10512039914") 
+      nama_umkm: "FunPlay Store",
+      alamat: "Jl.Sunter Jaya",
+      image_url:
+          "https://cf.shopee.co.id/file/4150b8ecc8b19ea3e3ac2c47e3845ef9",
+      no_telp: "081722445674",
+      daerah: "Jakarta Utara",
+      kategori: "Barang",
+      url_lokasi:
+          "https://shopee.co.id/animeyu.case?categoryId=100639&itemId=10512039914"),
   Umkm(
-     nama_umkm: "Your Bakery", 
-     alamat: "Jl.Rajawati Barat", 
-     image_url: "https://cf.shopee.co.id/file/73ac17276df825ef5c3dbe1e417ecf9b_tn", 
-     no_telp: "081379664933", 
-     daerah: "Jakarta Selatan", 
-     kategori: "Makanan"
-     url_lokasi: "https://www.tokopedia.com/honeybearbakery") 
+      nama_umkm: "Your Bakery",
+      alamat: "Jl.Rajawati Barat",
+      image_url:
+          "https://cf.shopee.co.id/file/73ac17276df825ef5c3dbe1e417ecf9b_tn",
+      no_telp: "081379664933",
+      daerah: "Jakarta Selatan",
+      kategori: "Makanan",
+      url_lokasi: "https://www.tokopedia.com/honeybearbakery"),
   Umkm(
-     nama_umkm: "BajuCeweNih!", 
-     alamat: "Jl.Gg. H . Mas'yud", 
-     image_url: "https://sc04.alicdn.com/kf/HTB1iX85jvxNTKJjy0Fjq6x6yVXaM.jpg", 
-     no_telp: "081255463820", 
-     daerah: "Jakarta Selatan", 
-     kategori: "Barang"
-     url_lokasi: "https://shopee.co.id/kamyno?categoryId=100017&itemId=4655049912") 
+      nama_umkm: "BajuCeweNih!",
+      alamat: "Jl.Gg. H . Mas'yud",
+      image_url: "https://sc04.alicdn.com/kf/HTB1iX85jvxNTKJjy0Fjq6x6yVXaM.jpg",
+      no_telp: "081255463820",
+      daerah: "Jakarta Selatan",
+      kategori: "Barang",
+      url_lokasi:
+          "https://shopee.co.id/kamyno?categoryId=100017&itemId=4655049912")
 ];
