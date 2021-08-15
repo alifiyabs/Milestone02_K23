0b0c53c5a0df1e6b0a0121e86894d63fc25dd79d
griendy15 <81350560+griendy15@users.noreply.github.com>
2021-08-15 13:40:10 +0700

    Update umkm.dart

--- milestone_lokalin/lib/umkm.dart
+++ milestone_lokalin/lib/umkm.dart
@@ -23,98 +23,98 @@ var umkm_list = [
       image_url: "https://sc01.alicdn.com/kf/HTB1uGjyeljTBKNjSZFuq6z0HFXaG/202439437/HTB1uGjyeljTBKNjSZFuq6z0HFXaG.jpg",
       no_telp: "081977453250",
       daerah: "Jakarta Selatan",
-      kategori: "Barang")
+      kategori: "Barang"),
   Umkm(
       nama_umkm: "Toko Batu Tulis Raden", 
       alamat: "Jl.Batu Tulis Kebun Kelapa", 
       image_url: "https://lh3.googleusercontent.com/p/AF1QipPYmvXqVgskSYIBtxmkdH99GB1nPF8oCzsztkBf=s1280-p-no-v1", 
       no_telp: "081397452633", 
       daerah: "Jakarta Pusat", 
-      kategori: "Barang")
+      kategori: "Barang"),
   Umkm(
       nama_umkm: "Toko Perkap Ling", 
       alamat: "Jl.Hasyim Ashari", 
       image_url: "https://awningjayalana.com/wp-content/uploads/2018/11/canopy-kain-1-394x258.png", 
       no_telp: "081634562480", 
       daerah: "Jakarta Pusat", 
-      kategori: "Barang")
+      kategori: "Barang"),
   Umkm(
       nama_umkm: "TaTem Geprek Pedas", 
       alamat: "Jl.Sonton 100", 
       image_url: "http://kbu-cdn.com/dk/wp-content/uploads/tahu-crispy-geprek.jpg", 
       no_telp: "081426557899", 
       daerah: "Jakarta Selatan", 
-      kategori: "Makanan")
+      kategori: "Makanan"),
   Umkm(
       nama_umkm: "Bakti Raya Cetak", 
       alamat: "Jl.Ciputat Raya (Depan Stasiun Kebayoran Lama)", 
       image_url: "https://iqbalazhari.com/wp-content/uploads/2016/10/peluang-usaha-toko-percetakan.jpg", 
       no_telp: "081645224788", 
       daerah: "Jakarta Selatan", 
-      kategori: "Barang")
+      kategori: "Barang"),
   Umkm(
       nama_umkm: "Romy Mebel", 
       alamat: "Jl.Pondok Pinang Raya", 
       image_url: "https://www.serbabandung.com/wp-content/uploads/2016/02/Toko-Mebel-Jati-Mulya.jpg", 
       no_telp: "081654228799", 
       daerah: "Jakarta Selatan", 
-      kategori: "Barang")
+      kategori: "Barang"),
   Umkm(
       nama_umkm: "Aneka Masakan Tempe Bu Ani", 
       alamat: "Jl.Utan Panjang", 
       image_url: "https://cdn.idntimes.com/content-images/community/2021/01/fromandroid-e578c77e38210897194db9cd8510663b.jpg", 
       no_telp: "081276945730", 
       daerah: "Jakarta Pusat", 
-      kategori: "Makanan")
+      kategori: "Makanan"),
   Umkm(
       nama_umkm: "Tempe Bacem Jakarta", 
       alamat: "Jl.Irian", 
       image_url: "https://doyanresep.com/wp-content/uploads/2020/06/resep-tempe-bacem.jpg", 
       no_telp: "081765248766", 
       daerah: "Jakarta Pusat", 
-      kategori: "Makanan")
+      kategori: "Makanan"),
   Umkm(
      nama_umkm: "Pusat Celana Ammi", 
      alamat: "Jl.Letjen Suprapto", 
      image_url: "http://asset-a.grid.id/crop/0x0:0x0/x/photo/2019/01/23/1368966398.jpg", 
      no_telp: "081423668799", 
      daerah: "Jakarta Pusat", 
-     kategori: "Barang")
+     kategori: "Barang"),
   Umkm(
      nama_umkm: "Konveksi Bu Adi", 
      alamat: "Jl.Serdang Kemayoran", 
      image_url: "https://alamatjalan.com/wp-content/uploads/2018/12/Toko-Harapan-Makassar-Toko-Kain-dan-Perlengpan-Menjahit-min.jpg", 
      no_telp: "081945772877", 
      daerah: "Jakarta Pusat", 
-     kategori: "Barang")
+     kategori: "Barang"),
   Umkm(
      nama_umkm: "YourShoes", 
      alamat: "Jl.Kemayoran", 
      image_url: "https://i2.wp.com/www.artforia.com/wp-content/uploads/2019/08/worm-tokyo-in-store.jpg?ssl=1", 
      no_telp: "081745367855", 
      daerah: "Jakarta Pusat", 
-     kategori: "Barang")
+     kategori: "Barang"),
   Umkm(
      nama_umkm: "Warung Ayam Bakar Adi", 
      alamat: "Jl.Mampang Prapatan 7", 
      image_url: "https://selerasa.com/wp-content/uploads/2015/12/images_daging_ayam-panggang-bumbu-spesial.jpg", 
      no_telp: "081465884622", 
      daerah: "Jakarta Selatan", 
-     kategori: "Makanan")
+     kategori: "Makanan"),
   Umkm(
      nama_umkm: "Kopi Seberang Kota", 
      alamat: "Jl.Warung Buncit III", 
      image_url: "https://majalah.ottencoffee.co.id/wp-content/uploads/2016/11/kedai-kopi.jpg", 
      no_telp: "081478994500", 
      daerah: "Jakarta Selatan", 
-     kategori: "Minuman")
+     kategori: "Minuman"),
   Umkm(
      nama_umkm: "Kids Friendly", 
      alamat: "Jl.UPT Kalibata Pulo", 
      image_url: "https://www.birdsnbees.co.id/blog/wp-content/uploads/2020/07/Toko-Jual-Baju-Anak-dan-Bayi-011.jpg", 
      no_telp: "081645448755", 
      daerah: "Jakarta Selatan", 
-     kategori: "Barang")
+     kategori: "Barang"),
   Umkm(
      nama_umkm: "Umar Peci Jakarta", 
      alamat: "Jl.H. Buang No.25", 
